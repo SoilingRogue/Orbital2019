7149b3d8734184cf8343ca69231ec69be4893fe6
uberSaiyan <yanwl.note@gmail.com>
2019-06-29 13:48:32 +0800

    - Added collision between player and fireballs - Fixed speed of fireballs

--- Basic Instinct/Assets/Scripts/Fireball.cs
+++ Basic Instinct/Assets/Scripts/Fireball.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour {
     private Vector3 direction;
-    private float travelSpeed = 2f;
+    private float travelSpeed = 10f;
+    private int damage = 20;
 
     public void setDirection(Vector3 direction) {
         this.direction = direction;
@@ -19,9 +20,26 @@ public class Fireball : MonoBehaviour {
         GameObject.Destroy(tempSphere);
 
         gameObject.AddComponent(typeof(MeshRenderer));
+
+        Rigidbody body = (Rigidbody)gameObject.AddComponent(typeof(Rigidbody));
+        body.useGravity = false;
+
+        Collider collider = (Collider)gameObject.AddComponent(typeof(SphereCollider));
+        collider.isTrigger = true;
     }   
 
     void Update() {
-        transform.position += direction * Time.deltaTime * travelSpeed;
+        // Normalize direction vector so that the speed of the projectile is independent
+        // of the distance between the enemy and the player.
+        transform.position += direction.normalized * Time.deltaTime * travelSpeed;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag("Player")) {
+            Debug.Log("Fireball Collided.");    
+            CharacterStats playerStats = obj.GetComponent<CharacterStats>();
+            playerStats.takeDamage(damage);
+        }
     }
 }
