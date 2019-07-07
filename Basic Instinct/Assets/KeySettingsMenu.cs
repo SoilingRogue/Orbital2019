d92dbe256d04580339d7d4ddcf84e166cf80492d
uberSaiyan <yanwl.note@gmail.com>
2019-07-07 17:54:20 +0800

    added start of key settings menu, incomplete

---
+++ Basic Instinct/Assets/KeySettingsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeySettingsMenu : MonoBehaviour {
+    // public TMP_Text buttonText;
+    private Button buttonPressed;
+    public void changeKey(Button self) {
+        Debug.Log("Key selected.");
+        buttonPressed = self;
+    }
+
+    void OnGUI() {
+        Event ev = Event.current;
+        if (ev.isKey) {
+            try {
+                TMP_Text buttonText = buttonPressed.transform.GetChild(0).GetComponent<TMP_Text>();
+                buttonText.text = ev.keyCode.ToString();
+                buttonPressed = null;
+            }
+            catch (NullReferenceException e) {
+                Debug.Log("No button selected.");
+                Debug.Log(e.StackTrace);
+                throw e;
+            }
+        }
+    }
+}
