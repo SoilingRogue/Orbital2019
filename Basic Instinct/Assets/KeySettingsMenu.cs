88caf28851633cf57bc0edf580c2f404e5d7f9f9
uberSaiyan <yanwl.note@gmail.com>
2019-07-14 00:01:39 +0800

    Improved menus

--- Basic Instinct/Assets/KeySettingsMenu.cs
+++ Basic Instinct/Assets/KeySettingsMenu.cs
@@ -6,25 +6,80 @@ using UnityEngine.UI;
 using TMPro;
 
 public class KeySettingsMenu : MonoBehaviour {
-    // public TMP_Text buttonText;
+    public TMP_Text errorMessage;
+    private int numberOfSkills = 4;
+    private List<string> keyCodeStringList;
+    private List<KeyCode> keyCodeList;
     private Button buttonPressed;
+
     public void changeKey(Button self) {
         Debug.Log("Key selected.");
         buttonPressed = self;
     }
 
+    public void saveKeys() {
+        Debug.Log("Saving keys.");
+
+        // Reinitialise both lists
+        keyCodeStringList = new List<string>(numberOfSkills);
+        keyCodeList = new List<KeyCode>(numberOfSkills);
+        
+        // Copy the button texts into a list of keycodes
+        for (int i = 0; i < numberOfSkills; i++) {
+            // Extract the set string from corresponding button
+            string keyCodeText = transform.GetChild(i).gameObject.transform.GetChild(0).GetComponent<TMP_Text>().text;
+
+            // Check if key code already in the list
+            if (keyAlreadyBinded(keyCodeText)) {
+                string errorText = "You cannot use the " + keyCodeText + " key";
+                displayErrorMessage(errorText);
+                // throw exception to prevent execution of the next two button functions
+                throw new ArgumentException(errorText);
+            }
+            else if (keyCodeStringList.Contains(keyCodeText)) {
+                string errorText = "You cannot use duplicate keys";
+                displayErrorMessage(errorText);
+                // throw exception to prevent execution of the next two button functions
+                throw new ArgumentException(errorText);
+            }
+            else {
+                keyCodeStringList.Add(keyCodeText);
+            }
+        }
+
+        // Converts the string to actual key code object
+        keyCodeStringList.ForEach(str => keyCodeList.Add((KeyCode)System.Enum.Parse(typeof(KeyCode), str)));
+    }
+
+    void displayErrorMessage(string errorText) {
+        errorMessage.text = errorText;
+        errorMessage.gameObject.SetActive(true);
+        CancelInvoke("hideErrorMessage");
+        Invoke("hideErrorMessage", 3);
+    }
+
+    void hideErrorMessage() {
+        errorMessage.gameObject.SetActive(false);
+    }
+
+    bool keyAlreadyBinded(string keyCodeText) {
+        List<string> bindedKeyTexts = new List<string>() {
+            "W",
+            "A",
+            "S",
+            "D",
+            "Escape"
+        };
+        return bindedKeyTexts.Contains(keyCodeText); 
+    }
+
     void OnGUI() {
         Event ev = Event.current;
         if (ev.isKey) {
-            try {
+            if (buttonPressed != null) {
                 TMP_Text buttonText = buttonPressed.transform.GetChild(0).GetComponent<TMP_Text>();
                 buttonText.text = ev.keyCode.ToString();
                 buttonPressed = null;
-            }
-            catch (NullReferenceException e) {
-                Debug.Log("No button selected.");
-                Debug.Log(e.StackTrace);
-                throw e;
             }
         }
     }
