03b664067da7d31b28442081325e766569e16aba
uberSaiyan <yanwl.note@gmail.com>
2019-07-20 11:52:41 +0800

    changes

--- Basic Instinct/Assets/Scripts/UI/StartMenu.cs
+++ Basic Instinct/Assets/Scripts/UI/StartMenu.cs
@@ -2,6 +2,13 @@
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
+    void Start() {
+        Animator[] animators = GameObject.FindObjectsOfType<Animator>();
+        foreach (Animator animator in animators) {
+            animator.WriteDefaultValues();
+        }
+    }
+
     public void loadNextScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
