4bf609a2e7d5441cbde5f8612fa80d87a451103c
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-07-20 23:49:33 +0800

    Skills GUI
    Customized skill icons and added cooldown visuals and script for cooldown

---
+++ Basic Instinct/Assets/Scripts/SkillSystem/Cooldown1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cooldown1 : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 5;
+    public string button;
+    protected bool isCooldown;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(button))
+        {
+            isCooldown = true;
+        }
+
+        if(isCooldown)
+        {
+            imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if(imageCooldown.fillAmount >= 1)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+    }
+}
---
+++ Basic Instinct/Assets/Scripts/SkillSystem/Cooldown2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cooldown2 : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 5;
+    public string button;
+    protected bool isCooldown;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(button) && !isCooldown)
+        {
+            isCooldown = true;
+            imageCooldown.fillAmount = 1;
+        }
+
+        if(isCooldown)
+        {
+            imageCooldown.fillAmount -= 1 / cooldown * Time.deltaTime;
+
+            if(imageCooldown.fillAmount == 0)
+            {
+                isCooldown = false;
+            }
+        }
+    }
+}
+
