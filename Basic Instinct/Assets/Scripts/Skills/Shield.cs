ec45b58a37574cd081d9ce59b0bb1fc189369fcf
uberSaiyan <yanwl.note@gmail.com>
2019-07-20 23:30:48 +0800

    changed skill system

--- Basic Instinct/Assets/Scripts/Skills/Shield.cs
+++ Basic Instinct/Assets/Scripts/Skills/Shield.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class Shield : Skill {
     private float duration;
+    private GameObject user;
+    public GameObject visualPrefab;
 
-    // Constructor
+    // protected override void initialise() {
+    //     Debug.Log("Executing Start in Shield");
+    //     cooldown = 5f;
+    //     duration = 1.5f;
+    //     user = GameObject.FindGameObjectWithTag("Player");
+    // }
+
     void Start() {
-        name = "Shield";
+        Debug.Log("Executing Start in Shield");
         cooldown = 5f;
-        // previousUseTime = -cooldown;
         duration = 1.5f;
+        user = GameObject.FindGameObjectWithTag("Player");
     }
 
-    protected override void use(GameObject character) {
-        CharacterStats stats = character.GetComponent<CharacterStats>();
-        stats.setInvulnerable(duration);
+    protected override void use() {
+        if (user != null) {
+            Debug.Log("User is not null");
+            // CharacterStats stats = user.GetComponent<CharacterStats>();
+            // if (stats != null) {
+            //     stats.setInvulnerable(duration);
+            // }
+        }
 
-        display(character);
+        display();
     }
 
-    private void display(GameObject character) {
-        GameObject bubble = new GameObject("Bubble");
-        bubble.AddComponent(typeof(ShieldBubble));
-        bubble.GetComponent<ShieldBubble>().character = character;
-        GameObject.Destroy(bubble, duration);
+    private void display() {
+        GameObject visual = Instantiate(visualPrefab, user.transform.position, user.transform.rotation);
+        Destroy(visual, duration);
+        // GameObject bubble = new GameObject("Bubble");
+        // bubble.AddComponent(typeof(ShieldBubble));
+        // bubble.GetComponent<ShieldBubble>().character = character;
+        // GameObject.Destroy(bubble, duration);
     }
 }
---
+++ Basic Instinct/Assets/Scripts/Skills/SkillManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour {
+    public Skill[] skills;
+    public KeyCode[] skillKeys;
+
+    void Start() {
+        skillKeys = new KeyCode[4];
+
+        for (int i = 0; i < 4; i++) {
+            skillKeys[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Skill_" + i));
+        }
+    }
+
+    void Update() {
+        if (Input.anyKeyDown) {
+            for (int i = 0; i < 4; i++) {
+                if (Input.GetKeyDown(skillKeys[i])) {
+                    Instantiate(skills[i].gameObject);
+                    skills[i].useSkill();
+                }
+            }
+        }
+    }
+}
