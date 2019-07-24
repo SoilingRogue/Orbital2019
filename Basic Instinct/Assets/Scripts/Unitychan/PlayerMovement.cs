5d618e5fd485d6aa5d1bbb4a9ba7a8149b425476
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-07-24 23:33:36 +0800

    Improved jumping
    Redid jumping animations and touched up on scripting for jumping

--- Basic Instinct/Assets/Scripts/Unitychan/PlayerMovement.cs
+++ Basic Instinct/Assets/Scripts/Unitychan/PlayerMovement.cs
@@ -21,26 +21,64 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {   
-        // Damaged/killed
-        if (Input.GetKeyDown("0"))
-        {
-            anim.Play("DAMAGED00", -1, 0f);
-        }
-        if (Input.GetKeyDown("1")) {
-            anim.Play("DAMAGED01", -1, 0f);
-        }
+        /* for movement */
+        inputH = Input.GetAxis("Horizontal"); // set this based on camera angle
+        inputV = Input.GetAxis("Vertical"); // set this based on camera angle
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+        float moveSpeed;
 
         if (IsGrounded())
         {
-            // Movement
-            if (anim.GetBool("moving"))
+            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
             {
                 // Running
                 anim.SetBool("run", Input.GetKey(KeyCode.LeftShift));
 
                 // Sliding
                 anim.SetBool("slide", Input.GetKey(KeyCode.LeftControl));
+            
+                if (anim.GetBool("run"))
+                {
+                    if (anim.GetBool("slide"))
+                    {
+                        moveSpeed = runSlideSpeed;
+                    }
+                    else
+                    {
+                    moveSpeed = runSpeed;
+                    }
+                }
+                else
+                {
+                    if (anim.GetBool("slide"))
+                    {
+                        moveSpeed = walkSlideSpeed;
+                    }
+                    else
+                    {
+                        moveSpeed = walkSpeed;
+                    }
+                }
+                Vector3 movementVector = new Vector3(inputH, 0 , inputV);
+                Quaternion camTurn = GetCameraTurn();
+                movementVector = camTurn * movementVector;
+                rBody.rotation = camTurn;
+                rBody.velocity += movementVector.normalized * moveSpeed;
+                Debug.Log("moving");
 
+                // rBody.velocity += inputH * Vector3.ProjectOnPlane(cam.transform.right, Vector3.up).normalized * moveSpeed * Time.deltaTime;            
+                // rBody.velocity += new Vector3(inputH, 0, inputV) * moveSpeed; // for debugging
+                // Debug.Log(rBody.position); // for debugging
+            }
+
+            // Damaged/killed
+            if (Input.GetKeyDown("0"))
+            {
+                anim.Play("DAMAGED00", -1, 0f);
+            }
+            if (Input.GetKeyDown("1")) {
+                anim.Play("DAMAGED01", -1, 0f);
             }
 
             // Emotes
@@ -50,80 +88,29 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKeyDown("t")) {
                 anim.Play("Thriller Part 2", -1, 0f);
             }
-
-            // Jumping
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                // anim.SetBool("jump", true);
-                anim.Play("Jump", -1, 0f);
-            }
         }
 
+        // GetComponent<Collider>().transform.position = gameObject.transform.position;
+    }
 
-
-        /* for movement */
-        inputH = Input.GetAxis("Horizontal"); // set this based on camera angle
-        inputV = Input.GetAxis("Vertical"); // set this based on camera angle
-        anim.SetFloat("inputH", inputH);
-        anim.SetFloat("inputV", inputV);
-        float moveSpeed;
-
-        // Setting boolean value for moving - for other scripts to access moving boolean
-        // if (inputV != 0 || inputH != 0)
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+    // AddForce is instantaneous, not frame dependent
+    void Update()
+    {
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            anim.SetBool("moving", true);
-            if (anim.GetBool("run"))
-            {
-                if (anim.GetBool("slide"))
-                {
-                    moveSpeed = runSlideSpeed;
-                }
-                else
-                {
-                    moveSpeed = runSpeed;
-                }
-            }
-            else
-            {
-                if (anim.GetBool("slide"))
-                {
-                    moveSpeed = walkSlideSpeed;
-                }
-                else
-                {
-                    moveSpeed = walkSpeed;
-                }
-            }
-
-            // if (anim.GetBool("jump") && IsGrounded())
-            if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-
-            {
-                // Vector3 jumpVector = new Vector3(0, Mathf.Sqrt(jumpHeight * -2f * gravity), 0);
-                // rBody.velocity = jumpVector;
-                Debug.Log("jumping");
-                rBody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-                // anim.SetBool("jump", false);
-            }
-
-            Vector3 movementVector = new Vector3(inputH, 0 , inputV);
-            Quaternion camTurn = GetCameraTurn();
-            movementVector = camTurn * movementVector;
-            rBody.rotation = camTurn;
-            rBody.velocity += movementVector.normalized * moveSpeed;
-
-            // rBody.velocity += inputH * Vector3.ProjectOnPlane(cam.transform.right, Vector3.up).normalized * moveSpeed * Time.deltaTime;            
-            // rBody.velocity += new Vector3(inputH, 0, inputV) * moveSpeed; // for debugging
-            // Debug.Log(rBody.position); // for debugging
+            // Vector3 jumpVector = new Vector3(0, Mathf.Sqrt(jumpHeight * -2f * gravity), 0);
+            // rBody.velocity = jumpVector;
+            Debug.Log("jumping");
+            anim.Play("Jump", -1, 0f);
+            // Delay adding of force to rBody to sync the animations
+            Invoke("Jump", 0.3f);
         }
-        else 
-        {
-            anim.SetBool("moving", false);
-        }
+    }
 
-        GetComponent<Collider>().transform.position = gameObject.transform.position;
-
+    private void Jump()
+    {
+        rBody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
     }
 
     private Quaternion GetCameraTurn()
@@ -131,7 +118,7 @@ public class PlayerMovement : MonoBehaviour
         return Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up);
     }
 
-    public bool IsGrounded()
+    private bool IsGrounded()
     {
         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
