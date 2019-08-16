2f6fca4f2c596b7c9d3aca69e998626499e83c50
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-08-16 10:16:52 +0800

    Tutorial Scripts

---
+++ Basic Instinct/Assets/Scripts/Tutorial/MouseTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTutorial : Tutorial
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void CheckIfHappening()
+    {
+
+        if (true)
+        {
+            TutorialManager.Instance.CompletedTutorial();
+        }
+    }
+}
---
+++ Basic Instinct/Assets/Scripts/Tutorial/trial/TutorialSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float startTimeBtwMonster;
+    private float timeBtwMonster;
+    public int numMonster;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwMonster <= 0 && numMonster> 0)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            timeBtwMonster = startTimeBtwMonster;
+            numMonster--;
+        }
+        else 
+        {
+            timeBtwMonster -= Time.deltaTime;
+        }
+    }
+}
