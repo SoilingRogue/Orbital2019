31497c6e5dfadd3f239fee2256b24f1fffb61a57
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-08-26 22:38:48 +0800

    PosterScene updates

---
+++ Basic Instinct/Assets/Scripts/Poster/CamControls.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControls : MonoBehaviour
+{
+    private float inputH, inputV, distToGround;
+    public float walkSpeed = 1f, runSpeed = 10f;
+    private float moveSpeed;
+    private Rigidbody rBody;
+    private bool run;
+    private Vector3 movementVector;
+    public Camera cam;
+    private int poseIndex = 0, currentPose = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {   
+        /* for movement */
+        inputH = Input.GetAxis("Horizontal"); // set this based on camera angle
+        inputV = Input.GetAxis("Vertical"); // set this based on camera angl
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+
+                // Running
+                run =  Input.GetKey(KeyCode.LeftShift);
+            
+                if (run)
+                {
+                    moveSpeed = runSpeed;
+                }
+                else
+                {
+                    moveSpeed = walkSpeed;
+                }
+
+                movementVector = new Vector3(inputH, 0 , inputV);
+                Quaternion camTurn = GetCameraTurn();
+                movementVector = camTurn * movementVector;
+
+                transform.rotation = Quaternion.LookRotation(movementVector);
+                transform.position += movementVector.normalized * moveSpeed;
+            }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+                transform.position += new Vector3(0, moveSpeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.position -= new Vector3(0, moveSpeed, 0);
+        }
+    }
+
+    private Quaternion GetCameraTurn()
+    {
+        return Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up);
+    }
+}
---
+++ Basic Instinct/Assets/Scripts/Poster/CameraOrbit1.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+ 
+public class CameraOrbit1 : MonoBehaviour
+{
+    // camera & pivot
+    protected Transform cam, pivot;
+    // storing rotations of camera pivot
+    protected Vector3 camRotation;
+    protected float camDistance = 3f;
+ 
+    // Edit settings to change orbit/scroll
+    /* 
+        Dampening - enables faster scrolling the further we are from the object, but slower scrolling when closer 
+        Sigmoidal effect when scrolling
+    */
+    public float mouseSensitivity = 4f, scrollSensitivity = 2f;
+    protected float orbitDampening = 10f, scrollDampening = 6f;
+ 
+    // Use this for initialization
+    void Start() {
+        this.cam = this.transform;
+        this.pivot = this.transform.parent;
+    }
+ 
+    // LateUpdate called after Update() on every game object in the scene, for rendering
+    void LateUpdate() {
+            // Rotation of the Camera based on Mouse Coordinates
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                camRotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+                camRotation.y -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+ 
+                // Clamp the y Rotation to horizon and not flipping over at the top
+                camRotation.y = Mathf.Clamp(camRotation.y, -80f, 80f);
+            }
+
+            // Zooming Input from our Mouse Scroll Wheel
+            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
+                scrollAmount *= (this.camDistance * 0.3f);
+ 
+                this.camDistance += scrollAmount * -1f;
+ 
+                this.camDistance = Mathf.Clamp(this.camDistance, -100f, 100f);
+            }
+ 
+        // Actual Camera Rig Transformations - has to be inside of LateUpdate()
+        // setting pitch and yaw for rotation
+        Quaternion QT = Quaternion.Euler(camRotation.y, camRotation.x, 0);
+        
+        // Lerp - linear interpolation btw current rotation at start of frame & animate towards target rotation
+        this.pivot.rotation = Quaternion.Lerp(this.pivot.rotation, QT, Time.deltaTime * orbitDampening);
+ 
+        if (this.cam.localPosition.z != this.camDistance * -1f)
+        {
+            this.cam.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this.cam.localPosition.z, this.camDistance * -1f, Time.deltaTime * scrollDampening));
+        }
+    }
+} 
